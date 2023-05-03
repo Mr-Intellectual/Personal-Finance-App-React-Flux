4e2f08a49970cffe14c79afc8f038b8e2cd42f0c
Reo Petersen <Reoapetersen@gmail.com>
2023-05-03 16:42:46 +0000

    Made new branch

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,10 +4,10 @@ import { Context } from "../store/appContext";
 
 export default function Home() {
   const { store, actions } = useContext(Context);
-  useEffect(() => {
-    actions.fetchData(); // Call the API when the component mounts
-  }, []);
-  let res = store.memoHolder
+  // useEffect(() => {
+  //   actions.fetchData(); // Call the API when the component mounts
+  // }, []);
+  // let res = store.memoHolder
 
 
   return (
@@ -15,8 +15,8 @@ export default function Home() {
       <div className="container-fluid py-3">
         <h1 className="display-5 fw-bold my-3" id="title">Welcome to the ToDo List!</h1>
         <p className="col-md-12 fs-4 mx-auto my-4" id="homeDisplay">
-          {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione magni vel aliquid, natus laudantium a beatae adipisci ab quam soluta pariatur, blanditiis totam corrupti sint suscipit! Dolor nam perferendis voluptatibus. */}
-          {res}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione magni vel aliquid, natus laudantium a beatae adipisci ab quam soluta pariatur, blanditiis totam corrupti sint suscipit! Dolor nam perferendis voluptatibus.
+          {/* {res} */}
         </p>
         <Link to="/todo">
           <button className="btn btn-primary btn-lg float-center mt-5 mb-3" type="button">
